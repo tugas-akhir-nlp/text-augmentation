48c3a5b0010901be73ca0bacdb629e5440ed61c9
Abdurrahman <13515024@std.stei.itb.ac.id>
2019-03-25 05:59:11 +0700

    modify debug printing in SentenceAugmenter and number masking now applied to ABSA data

--- src/SentenceAugmenter.py
+++ src/SentenceAugmenter.py
@@ -1,8 +1,8 @@
 # from __future__ import unicode_literals
 import numpy as np
 import pandas as pd
-from srilm import LM
-from augmentation import BasicTextAugmentation
+from src.srilm import LM
+from src.augmentation import BasicTextAugmentation
 
 class SentenceAugmenter:
 	def __init__(self, model_path):
@@ -14,10 +14,10 @@ class SentenceAugmenter:
 
 		df = pd.DataFrame(columns=['sentence', 'prev1', 'prev2', 'score'])
 		for next_word in augmented_words:
-			print(next_word)
+			# print(next_word)
 			next_df = pd.DataFrame(columns=['sentence', 'token', 'prev1', 'prev2', 'score'])
 			if type(next_word) is str:
-				print('processing string')
+				# print('processing string')
 				if df.size==0:
 					print
 					score = abs(self.lm.logprob_strings(next_word.encode(), list([])))
@@ -30,21 +30,23 @@ class SentenceAugmenter:
 						prev1 = row['token']
 						prev_score = row['score']
 						if pd.isna(prev1):
-							print('score from 1 word')
+							# print('score from 1 word')
 							score = abs(self.lm.logprob_strings(next_word.encode(), list([])))
 						elif pd.isna(prev2):
-							print('score from 2 word')
+							# print('score from 2 word')
 							score = abs(self.lm.logprob_strings(next_word.encode(), list([prev1.encode()])))
 						else:
-							print('score from 3 word')
-							print(next_word, prev1, prev2)
+							# print('score from 3 word')
+							# print(next_word, prev1, prev2)
 							score = abs(self.lm.logprob_strings(next_word.encode(), list([prev1.encode(), prev2.encode()])))
 						score = score*prev_score
 						next_df = next_df.append({'sentence': sentence, 'token': next_word, 'prev1': prev1, 'prev2': prev2, 'score': score}, ignore_index=True)
 
 				df = next_df
+				print(df)
+				
 			else: #list
-				print('processing list')
+				# print('processing list')
 				if df.size!=0:
 					for word in next_word:
 						for index, row in df.iterrows():
@@ -71,12 +73,13 @@ class SentenceAugmenter:
 
 				next_df.sort_values(by='score', inplace=True)
 				df = next_df.head(10)
+				print(df)
 
 		df.sort_values(by='score', inplace=True)
 		return df.iloc[0]['sentence']
 
 if __name__=='__main__':
-	path = '../preprocessing/count_order_3.bin'
+	path = 'model/orde3_smoothing.lm'
 	augmentation_rate = 0.3
 	raw_words = ['nyebelin','banget','bank','bni','tellernya','berbicara','dengan','kasar']
 
--- src/number_masking.py
+++ src/number_masking.py
@@ -1,40 +1,17 @@
-import re
-import multiprocessing as mp
-import timeit
+import json
+from src.preprocessing import number_masking
 
+if __name__=='__main__':
+	filenames = []
+    filenames.append('../targeted-absa/aspect/data/aspect_train.json')
+    filenames.append('../targeted-absa/aspect/data/aspect_test.json')
 
-# In[2]:
-filename = '../corpus_path.txt'
+    for filename in filenames:
+	    with open(filename, 'r') as file:
+	        aspect_data = json.load(file)
 
-with open('../corpus_path.txt', 'r') as file:
-    path = file.readlines()[0].strip()
-print(path)
+	    for datum in aspect_data:
+	        datum['text'] = number_masking(datum['text'])
 
-
-# In[3]:
-
-
-def number_masking(sentence):
-    sentence = re.sub(r'[0-9]+', r'<NUM>', sentence)
-    
-    return sentence
-
-
-start = timeit.default_timer()
-
-with open(path) as file:
-    corpus = file.readlines()
-
-for i in range(len(corpus)):
-    corpus[i] = number_masking(corpus[i])
-# pool = mp.Pool(processes=4)
-# results = [pool.apply(number_masking, args=(x,)) for x in corpus[:10]]
-# output = [p.get() for p in results]
-# print(output)
-
-stop = timeit.default_timer()
-execution_time = stop - start
-print('execution time: {}'.format(execution_time))
-
-with open(filename[:-4]+'_number_masked.txt', 'w') as file:
-    file.writelines(corpus)
+	    with open(filename[:-5]+'_masked.json', 'w') as file:
+	        json.dump(aspect_data, file)
